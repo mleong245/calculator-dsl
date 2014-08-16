b546ac92885398c8e7ab8eda08aab2008abfbddf
Matthew Leong <mleong94@gmail.com>
2014-08-15 16:45:58 -1000

    Implemented spec
    Wrote spec tests for proper functionality of the calculator

--- calculator_spec.rb
+++ calculator_spec.rb
@@ -5,9 +5,15 @@
 #   multiply 4
 #   divide 5
 #  end
+
+require './calculator'
  
 describe 'Calculator' do
-  
+   def calculate(&block)
+    subject.calculate(&block)
+  end
+
+
   describe "Addition operation" do
     it "returns correct values for positive numbers" do
       expect(
@@ -37,20 +43,74 @@ describe 'Calculator' do
       ).to eq(2)
     end
   end
- 
+
+
   describe "Subtraction operation" do
-    # Implement this
+    it "returns correct values for positive numbers" do
+      expect(
+        calculate do
+          subtract 5
+          subtract 3
+        end
+      ).to eq(-8)
+    end
+
+    it "returns correct values for negative numbers" do
+      expect(
+        calculate do
+          subtract -5
+          subtract -3
+        end
+      ).to eq(8)
+    end
+
+    it "returns correct values for positive and negative numbers" do
+      expect(
+        calculate do
+          subtract 5
+          subtract -3
+          subtract 2
+        end
+      ).to eq(6)
+    end
   end
- 
+
+
   describe "Division operation" do
-    # Implement this
+    it "returns correct values for division" do
+      expect(
+        calculate do
+          add 60
+          divide 2
+          divide 3
+        end
+      ).to eq(10)
+    end
+
+    it "raises a divide by zero error" do
+      expect{
+        calculate do
+          add 60
+          divide 0
+        end
+      }.to raise_error(ZeroDivisionError)
+    end
   end
- 
+
+
   describe "Multiplication operation" do
-    # Implement tests for this section
-    # Implement a test to check the divide by zero error
+    it "returns correct values for multiplication" do
+      expect(
+        calculate do
+          add 10
+          multiply 3
+          multiply 2
+        end
+      ).to eq(60)
+    end
   end
- 
+
+
   describe "Mixed operations" do
     it "returns correct values when mixing all operations" do
       expect(
